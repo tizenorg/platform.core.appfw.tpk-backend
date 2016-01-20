dd5c588b43f45ccf72eda923464cdab3d3eba58a
Arkadiusz Szulakiewicz <a.szulakiewi@partner.samsung.com>
2016-01-20 09:53:28 +0100

    Add StepParsePreload
    This change is dependant on the following patch:
    https://review.tizen.org/gerrit/57463
    
    Change-Id: I260435ac4dacad2f527dce1aac99afa5c6ed079e

---
+++ src/tpk/step/step_parse_preload.cc
@@ -0,0 +1,54 @@
+// Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE file.
+
+#include "tpk/step/step_parse_preload.h"
+
+#include <pkgmgr-info.h>
+#include <unistd.h>
+
+#include <boost/filesystem.hpp>
+
+#include <cstring>
+#include <cstdio>
+#include <string>
+
+#include "common/utils/file_util.h"
+
+namespace {
+const char* kPathManifestPrefix = "/usr/share/packages";
+}
+
+namespace common_installer {
+namespace parse {
+
+namespace bf = boost::filesystem;
+
+Step::Status StepParsePreload::process() {
+  const char* package_id = context_->pkgid.get().c_str();
+  const char* preload_manifest_val = context_->manifest_data.get()->preload;
+  if (!preload_manifest_val) {
+    LOG(ERROR) << "Could not find preload attribute in package:" << package_id;
+    return Status::PARSE_ERROR;
+  }
+
+  bf::path xml_path(getUserManifestPath(context_->uid.get()));
+  xml_path /= package_id;
+  xml_path += ".xml";
+
+  bool is_preloaded_candidate = bf::exists(xml_path) &&
+      (strcmp(preload_manifest_val, "true") == 0);
+
+  if (context_->request_type.get() == RequestType::ManifestDirectInstall) {
+    if (is_preloaded_candidate) {
+      LOG(ERROR) << "You're not authorized to install preload app: "
+          << package_id;
+      return Status::OPERATION_NOT_ALLOWED;
+    }
+  }
+
+  return Status::OK;
+}
+
+}  // namespace parse
+}  // namespace common_installer
---
+++ src/tpk/step/step_parse_preload.h
@@ -0,0 +1,33 @@
+/* 2014, Copyright © Eurogiciel Coporation, APACHE-2.0, see LICENSE file */
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef COMMON_STEP_STEP_PARSE_PRELOAD_H_
+#define COMMON_STEP_STEP_PARSE_PRELOAD_H_
+
+#include <manifest_parser/utils/logging.h>
+
+#include "common/installer_context.h"
+
+#include "common/step/step.h"
+
+namespace common_installer {
+namespace parse {
+
+class StepParsePreload : public Step {
+ public:
+  using Step::Step;
+
+  Status process() override;
+  Status clean() override { return Status::OK; }
+  Status undo() override { return Status::OK; }
+  Status precheck() override { return Status::OK; }
+
+  SCOPE_LOG_TAG(ParsePreload)
+};
+
+}  // namespace parse
+}  // namespace common_installer
+
+#endif  // COMMON_STEP_STEP_PARSE_PRELOAD_H_
