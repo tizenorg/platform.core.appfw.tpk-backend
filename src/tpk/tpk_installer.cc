8059838f641d59e5dd0195bea170f9adc2dcacb0
Lukasz Wartalowicz <l.wartalowic@samsung.com>
2016-01-20 14:38:56 +0100

    Requires: 	- https://review.tizen.org/gerrit/#/c/57334/
    Add StepRunParserPlugin to tpk-backend
    
    Change-Id: I53637957d88273a06e520548cec795df852b6c7a

--- src/tpk/tpk_installer.cc
+++ src/tpk/tpk_installer.cc
@@ -31,6 +31,7 @@
 #include <common/step/step_register_security.h>
 #include <common/step/step_rollback_deinstallation_security.h>
 #include <common/step/step_rollback_installation_security.h>
+#include <common/step/step_run_parser_plugins.h>
 #include <common/step/step_check_signature.h>
 #include <common/step/step_unregister_app.h>
 #include <common/step/step_unzip.h>
@@ -109,6 +110,7 @@ void TpkInstaller::InstallSteps() {
   AddStep<ci::security::StepRegisterSecurity>();
   AddStep<ci::tpk::StepConvertXml>();
   AddStep<ci::pkgmgr::StepRegisterApplication>();
+  AddStep<ci::pkgmgr::StepRunParserPlugin>(ci::ActionType::Install);
 }
 
 void TpkInstaller::UpdateSteps() {
@@ -131,6 +133,7 @@ void TpkInstaller::UpdateSteps() {
   AddStep<ci::filesystem::StepCreateIcons>();
   AddStep<ci::security::StepUpdateSecurity>();
   AddStep<ci::tpk::StepConvertXml>();
+  AddStep<ci::pkgmgr::StepRunParserPlugin>(ci::ActionType::Upgrade);
   AddStep<ci::pkgmgr::StepUpdateApplication>();
   /* TODO(jungh.yeon): this temporary step will be removed
   * when secondary parsing procedure has removed*/
@@ -147,6 +150,7 @@ void TpkInstaller::UninstallSteps() {
   AddStep<ci::filesystem::StepRemoveFiles>();
   AddStep<ci::filesystem::StepRemoveIcons>();
   AddStep<ci::security::StepRevokeSecurity>();
+  AddStep<ci::pkgmgr::StepRunParserPlugin>(ci::ActionType::Uninstall);
 }
 
 void TpkInstaller::ReinstallSteps() {
