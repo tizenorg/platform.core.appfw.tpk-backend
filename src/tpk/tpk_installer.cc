ca8f2db97843262a3b1014764f94922ce9c2eb60
Sangyoon Jang <s89.jang@samsung.com>
2016-03-02 17:11:42 +0900

    Check signature files when ManifestDirect mode
    Requires:
     - https://review.tizen.org/gerrit/60789
    
    Change-Id: Iba39601556a11a246cfd74a86a0a664845f7f780
    Signed-off-by: Sangyoon Jang <s89.jang@samsung.com>

--- src/tpk/tpk_installer.cc
+++ src/tpk/tpk_installer.cc
@@ -240,6 +240,7 @@ void TpkInstaller::ManifestDirectInstallSteps() {
       ci::parse::StepParseManifest::StoreLocation::NORMAL);
   AddStep<ci::tpk::StepParsePreload>();
   AddStep<tpk::pkgmgr::StepManifestAdjustment>();
+  AddStep<ci::security::StepCheckSignature>();
   AddStep<ci::security::StepPrivilegeCompatibility>();
   AddStep<tpk::security::StepCheckTpkBackgroundCategory>();
   AddStep<tpk::filesystem::StepCreateSymbolicLink>();
@@ -258,6 +259,7 @@ void TpkInstaller::ManifestDirectUpdateSteps() {
       ci::parse::StepParseManifest::StoreLocation::NORMAL);
   AddStep<ci::tpk::StepParsePreload>();
   AddStep<tpk::pkgmgr::StepManifestAdjustment>();
+  AddStep<ci::security::StepCheckSignature>();
   AddStep<ci::security::StepPrivilegeCompatibility>();
   AddStep<tpk::security::StepCheckTpkBackgroundCategory>();
   AddStep<tpk::filesystem::StepCreateSymbolicLink>();
