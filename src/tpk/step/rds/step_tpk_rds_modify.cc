66367b751648127086a0e0cd24231fd358bf63cc
Tomasz Iwanek <t.iwanek@samsung.com>
2016-04-04 14:10:22 +0200

    Implement reinstall request handling
    Requires:
     - https://review.tizen.org/gerrit/#/c/64662/
    
    To verify, run new smoke test:
     /usr/bin/tpk-backend-ut/smoke-test --gtest_filter="*Reinstall*"
    
    Change-Id: I59112f8a290674548f45a63988d5cceb1954d5ea

---
+++ src/tpk/step/rds/step_tpk_rds_modify.cc
@@ -0,0 +1,15 @@
+// Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache-2.0 license that can be
+// found in the LICENSE file.
+
+#include "tpk/step/rds/step_tpk_rds_modify.h"
+
+namespace tpk {
+namespace rds {
+
+std::string StepTpkRDSModify::GetAppPath() {
+  return context_->pkg_path.get().string();
+}
+
+}  // namespace rds
+}  // namespace tpk
