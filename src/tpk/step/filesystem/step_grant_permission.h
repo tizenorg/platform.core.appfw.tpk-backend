f802049bf6f0ced6f01308528848686344fdbded
Arkadiusz Szulakiewicz <a.szulakiewi@partner.samsung.com>
2016-05-23 16:04:31 +0200

    Grant permission for /bin and /lib path
    Change-Id: If375ff26f58cd562ba16e12ccaa11aa5c06bebef

---
+++ src/tpk/step/filesystem/step_grant_permission.h
@@ -0,0 +1,37 @@
+// Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache-2.0 license that can be
+// found in the LICENSE file.
+
+
+#ifndef TPK_STEP_FILESYSTEM_TPK_GRANT_PERMISSION_H_
+#define TPK_STEP_FILESYSTEM_TPK_GRANT_PERMISSION_H_
+
+#include <manifest_parser/utils/logging.h>
+
+#include <string>
+
+#include "common/installer_context.h"
+#include "common/step/step.h"
+
+namespace tpk {
+namespace filesystem {
+
+/**
+ * \brief Step responsible for granting permissions to /bin, /lib
+ */
+class StepTpkGrantPermission : public common_installer::Step {
+ public:
+  using Step::Step;
+
+  Status process() override;
+  Status undo() override { return Status::OK; }
+  Status clean() override { return Status::OK; }
+  Status precheck() override;
+
+  SCOPE_LOG_TAG(GrantPermission)
+};
+
+}  // namespace filesystem
+}  // namespace tpk
+
+#endif  // TPK_STEP_FILESYSTEM_TPK_GRANT_PERMISSION_H_
