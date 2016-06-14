73f54f3d4629849e778948868e2d8ed44b477227
Tomasz Iwanek <t.iwanek@samsung.com>
2016-06-14 10:55:48 +0200

    Handling external appdata directories
    Submit together:
     - https://review.tizen.org/gerrit/74805
     - https://review.tizen.org/gerrit/74806
    
    Change-Id: Ib352dcd4bf5990f729bc1157c612db4906e121c3

---
+++ src/tpk/external_dirs.cc
@@ -0,0 +1,65 @@
+// Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache-2.0 license that can be
+// found in the LICENSE file.
+
+#include "tpk/external_dirs.h"
+
+#include <common/pkgdir_tool_request.h>
+#include <common/privileges.h>
+#include <common/pkgmgr_registration.h>
+#include <common/shared_dirs.h>
+#include <common/utils/glist_range.h>
+#include <manifest_parser/utils/logging.h>
+
+#include <algorithm>
+
+namespace ci = common_installer;
+
+namespace tpk {
+
+bool HasExternalAppdataPrivilege(manifest_x* manifest) {
+  auto privileges = GListRange<char*>(manifest->privileges);
+  return std::find(privileges.begin(), privileges.end(),
+      std::string(common::privileges::kPrivForExternalAppData))
+          != privileges.end();
+}
+
+bool CreateExternalAppdataDirectories(const std::string& pkgid,
+                                      ci::RequestMode request_mode, uid_t uid) {
+  switch (request_mode) {
+    case ci::RequestMode::GLOBAL: {
+      LOG(DEBUG) << "Creating external directories for all users";
+      ci::RequestCreateExternalDirectories(pkgid);
+      break;
+    }
+    case ci::RequestMode::USER: {
+      LOG(DEBUG) << "Creating external directories for user: " << uid;
+      ci::PerformExternalDirectoryCreationForUser(uid, pkgid);
+      break;
+    }
+  }
+  return true;
+}
+
+bool DeleteExternalAppdataDirectories(const std::string& pkgid,
+                                      ci::RequestMode request_mode, uid_t uid) {
+  switch (request_mode) {
+    case ci::RequestMode::GLOBAL: {
+      LOG(DEBUG) << "Removing external directories for all users";
+      ci::RequestDeleteExternalDirectories(pkgid);
+      break;
+    }
+    case ci::RequestMode::USER: {
+      // if package is globally installed, leave directories
+      if (ci::IsPackageInstalled(pkgid, GLOBAL_USER))
+        return true;
+
+      LOG(DEBUG) << "Removing external directories for user: " << uid;
+      ci::PerformExternalDirectoryDeletionForUser(uid, pkgid);
+      break;
+    }
+  }
+  return true;
+}
+
+}  // namespace tpk
