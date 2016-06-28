0ac13d3f2d11c3c1dbdf12e01c87d90225c0ccfa
Sangyoon Jang <s89.jang@samsung.com>
2016-06-28 15:56:04 +0900

    Remove StepAcquireExternalStep from some Request type
    This step does not support following steps:
     - Clear, Reinstall, Recovery, ManifestDirectInstall/Update,
       MountInstall/Update
    
    Change-Id: I72bcf3a8b6da916b1c1f53e09124a87b796b9aad
    Signed-off-by: Sangyoon Jang <s89.jang@samsung.com>

--- src/tpk/tpk_installer.cc
+++ src/tpk/tpk_installer.cc
@@ -257,7 +257,6 @@ void TpkInstaller::ReinstallSteps() {
   AddStep<ci::pkgmgr::StepKillApps>();
   AddStep<ci::backup::StepBackupManifest>();
   AddStep<ci::backup::StepBackupIcons>();
-  AddStep<ci::filesystem::StepAcquireExternalStorage>();
   AddStep<ci::rds::StepRDSParse>();
   AddStep<tpk::rds::StepTpkRDSModify>();
   AddStep<ci::filesystem::StepUpdateTep>();
@@ -348,7 +347,6 @@ void TpkInstaller::MountInstallSteps() {
   AddStep<ci::security::StepPrivilegeCompatibility>();
   AddStep<tpk::security::StepCheckTpkBackgroundCategory>();
   AddStep<ci::security::StepRollbackInstallationSecurity>();
-  AddStep<ci::filesystem::StepAcquireExternalStorage>();
   AddStep<ci::mount::StepMountInstall>();
   AddStep<tpk::filesystem::StepTpkPreparePackageDirectory>();
   AddStep<ci::filesystem::StepCopyTep>();
@@ -387,7 +385,6 @@ void TpkInstaller::MountUpdateSteps() {
   AddStep<ci::pkgmgr::StepKillApps>();
   AddStep<ci::backup::StepBackupManifest>();
   AddStep<ci::backup::StepBackupIcons>();
-  AddStep<ci::filesystem::StepAcquireExternalStorage>();
   AddStep<ci::mount::StepMountUpdate>();
   AddStep<tpk::filesystem::StepTpkPreparePackageDirectory>();
   AddStep<ci::filesystem::StepUpdateTep>();
@@ -408,7 +405,6 @@ void TpkInstaller::ManifestDirectInstallSteps() {
   AddStep<ci::configuration::StepParseManifest>(
       ci::configuration::StepParseManifest::ManifestLocation::INSTALLED,
       ci::configuration::StepParseManifest::StoreLocation::NORMAL);
-  AddStep<ci::filesystem::StepAcquireExternalStorage>();
   AddStep<tpk::filesystem::StepCheckPkgDirPath>();
   AddStep<tpk::configuration::StepParsePreload>();
   AddStep<ci::configuration::StepCheckTizenVersion>();
@@ -432,7 +428,6 @@ void TpkInstaller::ManifestDirectUpdateSteps() {
   AddStep<ci::configuration::StepParseManifest>(
       ci::configuration::StepParseManifest::ManifestLocation::INSTALLED,
       ci::configuration::StepParseManifest::StoreLocation::NORMAL);
-  AddStep<ci::filesystem::StepAcquireExternalStorage>();
   AddStep<tpk::configuration::StepParsePreload>();
   AddStep<ci::configuration::StepCheckTizenVersion>();
   AddStep<tpk::pkgmgr::StepManifestAdjustment>();
@@ -454,7 +449,6 @@ void TpkInstaller::ClearSteps() {
   AddStep<ci::configuration::StepParseManifest>(
       ci::configuration::StepParseManifest::ManifestLocation::INSTALLED,
       ci::configuration::StepParseManifest::StoreLocation::NORMAL);
-  AddStep<ci::filesystem::StepAcquireExternalStorage>();
   AddStep<ci::filesystem::StepClearData>();
 }
 
