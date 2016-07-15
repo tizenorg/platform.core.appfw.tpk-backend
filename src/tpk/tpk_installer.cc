e1319e0bee52e30eff419fa13b6caa5d4ef33e53
Tomasz Iwanek <t.iwanek@samsung.com>
2016-07-15 10:14:11 +0200

    Reorder steps for delta update for external installation
    Requires:
     - https://review.tizen.org/gerrit/80277
    
    To verify, check that delta update mode works for external storage
    
    Change-Id: I5e52418755d4f254c449342df67c446a11eb8aac

--- src/tpk/tpk_installer.cc
+++ src/tpk/tpk_installer.cc
@@ -19,6 +19,8 @@
 #include <common/step/filesystem/step_create_per_user_storage_directories.h>
 #include <common/step/filesystem/step_create_storage_directories.h>
 #include <common/step/filesystem/step_delta_patch.h>
+#include <common/step/filesystem/step_disable_external_mount.h>
+#include <common/step/filesystem/step_enable_external_mount.h>
 #include <common/step/filesystem/step_move_installed_storage.h>
 #include <common/step/filesystem/step_recover_files.h>
 #include <common/step/filesystem/step_recover_icons.h>
@@ -277,7 +279,9 @@ void TpkInstaller::DeltaSteps() {
       ci::configuration::StepParseManifest::ManifestLocation::INSTALLED,
       ci::configuration::StepParseManifest::StoreLocation::BACKUP);
   AddStep<ci::configuration::StepCheckTizenVersion>();
+  AddStep<ci::filesystem::StepEnableExternalMount>();
   AddStep<ci::filesystem::StepDeltaPatch>();
+  AddStep<ci::filesystem::StepDisableExternalMount>();
   AddStep<ci::security::StepCheckSignature>();
   AddStep<ci::security::StepPrivilegeCompatibility>();
   AddStep<tpk::security::StepCheckTpkBackgroundCategory>();
